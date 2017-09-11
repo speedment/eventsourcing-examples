43627fa8dd229cdc0fd2c21afcb0d44825457521
pyknic <emil@speedment.com>
2017-09-11 14:14:53 +0200

    config-service: Remove eureka from config service and fix properties

--- config-service/src/main/java/com/speedment/example/eventsourcing/config/ConfigApp.java
+++ config-service/src/main/java/com/speedment/example/eventsourcing/config/ConfigApp.java
@@ -2,19 +2,16 @@ package com.speedment.example.eventsourcing.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author Emil Forslund
  * @since  1.0.0
  */
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApp {
-
     public static void main(String... args) {
         SpringApplication.run(ConfigApp.class, args);
     }
-
 }

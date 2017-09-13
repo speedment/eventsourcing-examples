f2a9dca988c4f4fa5dd8c227a7177c0630fce0ee
pyknic <emil@speedment.com>
2017-09-13 10:06:51 +0200

    edge-service: Switch over to Zuul as edge service

--- edge-service/src/main/java/com/speedment/example/eventsourcing/edge/EdgeApp.java
+++ edge-service/src/main/java/com/speedment/example/eventsourcing/edge/EdgeApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 @EnableZuulProxy
 @EnableZuulWebSocket
 @EnableWebSocketMessageBroker
+@EnableWebSecurity
 public class EdgeApp {
 //
 //    @Bean
